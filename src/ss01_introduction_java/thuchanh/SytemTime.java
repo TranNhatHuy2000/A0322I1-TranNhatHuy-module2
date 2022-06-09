5b4d03e7b1bd9d2869acbcee6a3e6585d3cdc141
TranNhatHuy2000 <binbeo90@gmail.com>
2022-06-09 11:37:24 +0700

    push th1_SystemTime

---
+++ src/ss01_introduction_java/thuchanh/SytemTime.java
@@ -0,0 +1,10 @@
+package ss01_introduction_java.thuchanh;
+
+import java.util.Date;
+
+public class SytemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Date now: "+now);
+    }
+}
