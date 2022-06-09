c2a0ec291504c6ed4ff7b54d9bd1dbf317b6ab7f
TranNhatHuy2000 <binbeo90@gmail.com>
2022-06-09 11:50:34 +0700

    push th2_Variable

---
+++ src/ss01_introduction_java/thuchanh/Variable.java
@@ -0,0 +1,20 @@
+package ss01_introduction_java.thuchanh;
+
+public class Variable {
+    public static void main(String[] args) {
+        byte i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println(
+                i + "\n"+
+                f + "\n"+
+                d + "\n"+
+                b + "\n"+
+                c + "\n"+
+                s + "\n"
+        );
+    }
+}
