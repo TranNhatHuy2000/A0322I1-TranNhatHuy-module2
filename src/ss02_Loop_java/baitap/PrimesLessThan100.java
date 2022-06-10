8e98881d29eaf43149d90a7a3d52b4748533bb9c
TranNhatHuy2000 <binbeo90@gmail.com>
2022-06-10 11:33:30 +0700

    push bt3_PrimeLessThan100

--- src/ss02_Loop_java/baitap/PrimesLessThan100.java
+++ src/ss02_Loop_java/baitap/PrimesLessThan100.java
@@ -1,15 +1,11 @@
 package ss02_Loop_java.baitap;
 
-import java.util.Scanner;
-
-public class CountPrime {
+public class PrimesLessThan100 {
     public static void main(String[] args) {
-        int number, N=2;
+        int number =100, N=2;
         boolean check = true;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Nhập số lượng số nguyên tố cần in: ");
-        number = input.nextByte();
-        for (int i = 1; i <= number;){
+        System.out.print("Các số nguyên tố <100 là: ");
+        for (int i = 1; i <= number;i++){
             for (int j = 2 ;j <= Math.sqrt(N) ;j++) {
                 if (N%j==0) {
                     check = false;
@@ -22,7 +18,6 @@ public class CountPrime {
             }
             check = true;
             N++;
-            }
         }
     }
-
+}
