c36713c2c999f4a1e4fd9bc9fcc74889a2597d98
TranNhatHuy2000 <binbeo90@gmail.com>
2022-06-12 17:37:08 +0700

    push bt2_ÍnesertElementArray

---
+++ src/ss03_Array_Method/baitap/bt2_InsertElementArray.java
@@ -0,0 +1,29 @@
+package ss03_Array_Method.baitap;
+
+import java.util.Scanner;
+
+public class bt2_InsertElementArray {
+    public static void main(String[] args) {
+        int[] array = {4,8,6,5,2,6,5,9,0,0,0,0,0};
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhâp phần tử muốn thêm vào mảng:");
+        int insert = input.nextInt();
+        System.out.print("Nhập vị trí thêm vào trong mảng:");
+        int index = input.nextInt();
+
+        if(index <= -1 && index >= array.length-1){
+            System.out.println("Không chèn được phần tử trong mảng");
+        }else{
+            for(int i = index; i <array.length;i++){
+                array[i] = array[index-1];
+            }
+            array[index] = insert;
+            System.out.print("Mảng sau khi thêm là");
+            for (int element : array){
+                System.out.print(element+ " ");
+            }
+        }
+
+    }
+
+}
