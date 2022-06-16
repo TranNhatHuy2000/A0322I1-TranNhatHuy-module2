02778ef69e01d20ff6598547e67de5cb65e61e48
TranNhatHuy2000 <binbeo90@gmail.com>
2022-06-16 21:56:12 +0700

    push th2_StaticProperty_CarClass

---
+++ src/ss05_AccesModifier_StaticMethod_StaticProperty/thuchanh/Car.java
@@ -0,0 +1,23 @@
+package ss05_AccesModifier_StaticMethod_StaticProperty.thuchanh;
+
+public class Car {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+
+        System.out.println(Car.numberOfCars);
+
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+
+        System.out.println(Car.numberOfCars);
+    }
+}
