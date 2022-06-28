20bd8611ef5f346bea8b5d5100820d6236856a0a
TranNhatHuy2000 <binbeo90@gmail.com>
2022-06-28 21:25:51 +0700

    push th3_Cylinder

---
+++ src/ss08_CleanCode_Refactoring/thuchanh/th3_apj_cylinder/Cylinder.java
@@ -0,0 +1,18 @@
+package ss08_CleanCode_Refactoring.thuchanh.th3_apj_cylinder;
+
+public class Cylinder {
+    public static double getVolume(int radius, int height) {
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
+    }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
+    }
+}
