5a87cf33c5613a226180223b5a7f99ef16adb637
TranNhatHuy2000 <binbeo90@gmail.com>
2022-07-24 14:36:00 +0700

    th2_ArrayIndexOutOfBoundException

---
+++ src/ss14_Exception_debug/thuchanh/ArrayIndexOutOfBoundsException/Main.java
@@ -0,0 +1,19 @@
+package ss14_Exception_debug.thuchanh.ArrayIndexOutOfBoundsException;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayExample arrExample = new ArrayExample();
+        Integer[] arr = arrExample.createRandom();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
